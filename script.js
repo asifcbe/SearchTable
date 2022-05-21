3a0b33700ed20f01959a1eb779ac052d8b265f16
Asif Abdul Jaleel <97680295+asifcbe@users.noreply.github.com>
2022-05-21 14:07:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var url =
-  "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D";
 var usdata = [];
 var tbhtml = "";
 var a = [];
 var j;
-$.get(url, function (res) {
+$.get( "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D"
+  ,
+      function (res) {
   usdata = res;
 
   res.map((res, i) => {
